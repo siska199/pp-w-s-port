8a19fcc7cff184acd25c522875e75d5299998f21
siska199 <siska.apriana.1703226@students.um.ac.id>
2024-09-25 15:00:53 +0700

    enhance image shadow in project

--- src/components/modules/portofolio/project-section.tsx
+++ src/components/modules/portofolio/project-section.tsx
@@ -5,7 +5,7 @@ import Image from "@components/ui/image";
 import { projects } from "@lib/data/dummy";
 
 const ProjectSection = () => {
-  return <div className="min-h-[calc(100%-5rem)] text-white my-8 space-y-10 ">
+  return <div className="min-h-[calc(100%-5rem)] text-white my-8  ">
         <h3 className="text-heading-05 md:text-heading-03 text-center font-bold font-bubblegum-sans">Projects</h3>
         <div className="grid grid-cols-1 gap-8 max-w-[50rem] mx-auto">
         {
@@ -24,9 +24,9 @@ interface TPropsCardProject {
 }
 const CardProject = (props : TPropsCardProject)=>{
     const {thumbnail,} = props
-    return <div className="overflow-hidden border-b py-8 ">
-        <Image src={thumbnail} className=" h-[15rem] border-1 shadow-2xl"/>
-        <div className="space-y-4 p-4">
+    return <div className="overflow-hidden border-b px-4 py-8 ">
+        <Image src={thumbnail} className=" h-[10rem]  md:h-[15rem] border-1 shadow-image-arise border-gray-500"/>
+        <div className="space-y-4 py-4">
             <h5 className=" text-white text-body-large font-bold">{props.title}</h5>
             <p className="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum consequatur suscipit illo, enim accusantium repellat?</p>
             <div className="flex gap-2 flex-wrap">
