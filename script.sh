5188216f71b1cf93d262ca87b6229cd88208b929
Siska Apriana Rifianti <siska.apriana.rifianti199@gmail.com>
2024-10-31 06:21:31 +0700

    updte

--- script.sh
+++ script.sh
@@ -7,11 +7,13 @@ VOLUME_PATH="$(pwd):/kyuuchan-199/app-s-port-client"
 NODE_MODULES_VOLUME="/kyuuchan-199/app-s-port-client/node_modules"
 
 # Check if the container already exists
-if [ "$(docker ps -a -q -f name=$CONTAINER_NAME)" ]; then
-    echo "Container $CONTAINER_NAME exists. Starting it..."
-    docker start $CONTAINER_NAME
-else
-    echo "Container $CONTAINER_NAME does not exist. Building the image and creating the container..."
-    docker build --no-cache -t $IMAGE_NAME .
-    docker run --name $CONTAINER_NAME -p $PORT:$PORT -v "$VOLUME_PATH" -v "$NODE_MODULES_VOLUME" $IMAGE_NAME
-fi
+# if [ "$(docker ps -a -q -f name=$CONTAINER_NAME)" ]; then
+#     echo "Container $CONTAINER_NAME exists. Starting it..."
+#     docker start $CONTAINER_NAME
+# else
+#     echo "Container $CONTAINER_NAME does not exist. Building the image and creating the container..."
+# fi
+
+docker rmi $IMAGE_NAME
+docker build --no-cache -t $IMAGE_NAME .
+docker run -d --name $CONTAINER_NAME -p $PORT:$PORT -v "$VOLUME_PATH" -v "$NODE_MODULES_VOLUME" $IMAGE_NAME
